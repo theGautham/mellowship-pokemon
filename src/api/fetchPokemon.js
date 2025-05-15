f6b4e6b9de074b413f4052e415e3934e12982bd0
Gautham V <gauthamvr2005@gmail.com>
2025-05-15 16:12:34 +0530

    grid v2

--- src/api/fetchPokemon.js
+++ src/api/fetchPokemon.js
@@ -1,11 +1,11 @@
 export const fetchPokemon = async () => {
-    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=50');
-    const data = await response.json();
-    const pokemonDetails = await Promise.all(
-      data.results.map(async (pokemon) => {
-        const res = await fetch(pokemon.url);
-        return res.json();
-      })
-    );
-    return pokemonDetails;
-  };
+  const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=50');
+  const data = await response.json();
+  const pokemonDetails = await Promise.all(
+    data.results.map(async (pokemon) => {
+      const res = await fetch(pokemon.url);
+      return res.json();
+    })
+  );
+  return pokemonDetails;
+};
